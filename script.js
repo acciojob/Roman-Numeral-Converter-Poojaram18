2e2d97e2294cb8751057cb70f76dad6da072d686
PoojaRamaraj <119429335+Poojaram18@users.noreply.github.com>
2023-01-17 09:10:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ function convertToRoman(num) {
     };
 
   //your code here
-	var ans ='';
-	for(vae key in obj){
-	while(num>=obj[key]){
-		ans+= key;
-		num -= obj[key];
-	}
-	}
-	return ans;
+var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+  for ( i in lookup ) {
+    while ( num >= lookup[i] ) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
